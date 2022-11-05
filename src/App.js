73ef676a93a5faae17d88b58ec78694829a5719b
quochung9920 <quochung9920@gmail.com>
2022-11-05 15:35:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+
+    axios.get('https://63661f2379b0914b75c9ad6a.mockapi.io/api/employees', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': '*'
+      }
+    }).then(res => {
+      setUsers(res.data)
+      console.log(res.data)
+    })
+
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Nguyễn Quốc Hưng</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1>Test call API successfully</h1>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>  
+    </React.Fragment>
+  )
 }
 
 export default App;
